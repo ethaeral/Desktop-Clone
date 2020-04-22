84a73a1cd5fe622453abf48770591b4326d1b4f9
richanynguon <alina.nguon@gmail.com>
2020-04-22 15:38:59 -0400

    import assests for side nav iconsand create hover css

--- src/components/vscodeCopy/components/mainViewport/components/sideUtlityBar/SideUtilityBar.js
+++ src/components/vscodeCopy/components/mainViewport/components/sideUtlityBar/SideUtilityBar.js
@@ -1,6 +1,42 @@
 import React from "react";
-import { SideUtility } from "./styles/sideUtilityStyles";
+import {
+	SideUtility,
+	SideIconHolder,
+	SideIcon,
+} from "./styles/sideUtilityStyles";
+
+import bug from "../../../../../../assets/sideNav/bug.png";
+import bugActive from "../../../../../../assets/sideNav/bugActive.png";
+import bugHover from "../../../../../../assets/sideNav/bugHover.png";
+import extension from "../../../../../../assets/sideNav/extension.png";
+import extensionActive from "../../../../../../assets/sideNav/extensionActive.png";
+import extensionHover from "../../../../../../assets/sideNav/extensionHover.png";
+import gitbranch from "../../../../../../assets/sideNav/gitbranch.png";
+import gitbranchActive from "../../../../../../assets/sideNav/gitbranchActive.png";
+import gitbranchHover from "../../../../../../assets/sideNav/gitbranchHover.png";
+import projects from "../../../../../../assets/sideNav/projects.png";
+import projectsActive from "../../../../../../assets/sideNav/projectsActive.png";
+import projectsHover from "../../../../../../assets/sideNav/projectHover.png";
+import search from "../../../../../../assets/sideNav/search.png";
+import searchActive from "../../../../../../assets/sideNav/searchActive.png";
+import searchHover from "../../../../../../assets/sideNav/searchHover.png";
+import setting from "../../../../../../assets/sideNav/setting.png";
+import settingActive from "../../../../../../assets/sideNav/settingActive.png";
+import settingHover from "../../../../../../assets/sideNav/settingHover.png";
 
 export default function SideUtilityBar() {
-	return <SideUtility>SideUtilityBar</SideUtility>;
+	return (
+		<SideUtility>
+			<SideIconHolder>
+				<SideIcon image={search} active={searchActive} hover={searchHover} />
+				<SideIcon image={gitbranch} active={gitbranchActive} hover={gitbranchHover} />
+				<SideIcon image={bug} active={bugActive} hover={bugHover} />
+				<SideIcon image={extension} active={extensionActive} hover={extensionHover} />
+				<SideIcon image={projects} active={projectsActive} hover={projectsHover} />
+			</SideIconHolder>
+			<SideIconHolder>
+				<SideIcon image={setting} active={settingActive} hover={settingHover} />
+			</SideIconHolder>
+		</SideUtility>
+	);
 }
--- src/components/vscodeCopy/components/mainViewport/components/sideUtlityBar/styles/sideUtilityStyles.js
+++ src/components/vscodeCopy/components/mainViewport/components/sideUtlityBar/styles/sideUtilityStyles.js
@@ -3,4 +3,33 @@ import styled from "styled-components";
 export const SideUtility = styled.div`
 	background-color: #343746;
 	width: 3.25em;
+	display: flex;
+	justify-content: space-between;
+	flex-direction: column;
+	padding: 0.5em 0.5em 0em 0.5em;
+`;
+
+export const SideIconHolder = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+`;
+
+export const SideIcon = styled.div`
+	width: 100%;
+	height: 29px;
+	background-position: center;
+	background-image: ${(props) => `url(${props.image})`};
+	background-repeat: no-repeat;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 1.5em;
+	&:last-child {
+		margin-bottom: 0.5em;
+	}
+	&:hover{
+		background-image: ${(props) => `url(${props.hover})`};
+	}
 `;
