33a7e29f492883ab013eb4cd7f9f2faed057e236
richanynguon <alina.nguon@gmail.com>
2020-04-22 21:26:52 -0400

    remove unneccessary dependencies and create components for extended utility bar

--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/ExpandedUtilityBar.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/ExpandedUtilityBar.js
@@ -1,7 +1,15 @@
 import React, { useContext } from "react";
+
 import { ExpandedUtility } from "./styles/expandedUtilityStyles";
 import WindowsContext from "../../../../../../modules/windowContext";
+
+import Branch from './components/branch/Branch'
+import Bug from './components/bug/Bug'
+import Ext from './components/ext/Ext'
+import Projects from './components/projects/Projects'
+import Search from './components/search/Search'
+
 export default function ExpandedUtilityBar() {
 	const { statefulWindows, setWindowState } = useContext(WindowsContext);
-	return <ExpandedUtility>ExpandedUtilityBar</ExpandedUtility>;
+	return <ExpandedUtility></ExpandedUtility>;
 }
---
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/bug/Bug.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Bug() {
+  return (
+    <div>
+      Bug
+    </div>
+  )
+}
