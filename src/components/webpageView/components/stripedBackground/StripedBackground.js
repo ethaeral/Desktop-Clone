961daeca2c5d0ccf08c5f2c11def3208d949e5be
richanynguon <alina.nguon@gmail.com>
2020-04-25 11:14:09 -0400

    create stripe animation

--- src/components/webpageView/components/stripedBackground/StripedBackground.js
+++ src/components/webpageView/components/stripedBackground/StripedBackground.js
@@ -1,30 +1,72 @@
 import React from "react";
 
+import {
+	CreamBottom,
+	CreamTop,
+	DarkBlueBottom,
+	DarkBlueTop,
+	LightOrangeBottom,
+	LightOrangeTop,
+	LowerContainer,
+	OrangeBottom,
+	OrangeTop,
+	StripedContainer,
+	TealBottom,
+	TealTop,
+	UpperContainer,
+	WhiteBottom,
+	WhiteTop,
+	RevealAnimation,
+} from "./styles/stripedStyles";
+
 export default function StripedBackground() {
 	return (
-		<div>
-			<div>
-				<div>
-					<div>
-						<div>
-							<div>
-								<div></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-      <div>
-				<div>
-					<div>
-						<div>
-							<div>
-								<div></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+		<StripedContainer>
+			<RevealAnimation>
+			</RevealAnimation>
+				<UpperContainer>
+					<DarkBlueTop>
+						<WhiteTop>
+							<TealTop>
+								<WhiteTop>
+									<CreamTop>
+										<WhiteTop>
+											<LightOrangeTop>
+												<WhiteTop>
+													<OrangeTop>
+														<WhiteTop></WhiteTop>
+													</OrangeTop>
+												</WhiteTop>
+											</LightOrangeTop>
+										</WhiteTop>
+									</CreamTop>
+								</WhiteTop>
+							</TealTop>
+						</WhiteTop>
+					</DarkBlueTop>
+				</UpperContainer>
+				<LowerContainer>
+					<OrangeBottom>
+						<WhiteBottom>
+							<LightOrangeBottom>
+								<WhiteBottom>
+									<CreamBottom>
+										<WhiteBottom>
+											<TealBottom>
+												<WhiteBottom>
+													<DarkBlueBottom>
+														<WhiteBottom></WhiteBottom>
+													</DarkBlueBottom>
+												</WhiteBottom>
+											</TealBottom>
+										</WhiteBottom>
+									</CreamBottom>
+								</WhiteBottom>
+							</LightOrangeBottom>
+						</WhiteBottom>
+					</OrangeBottom>
+				</LowerContainer>
+
+		</StripedContainer>
 	);
 }
---
+++ src/components/webpageView/components/stripedBackground/styles/stripedStyles.js
@@ -0,0 +1,141 @@
+import styled from "styled-components";
+
+export const StripedContainer = styled.div`
+	height: 100%;
+	width: 100%;
+	position: absolute;
+	background-color: #fff;
+	overflow: hidden;
+`;
+export const RevealAnimation = styled.div`
+	height: 100%;
+	width: 100%;
+	position: absolute;
+	background-color: #fff;
+	z-index: 1;
+	@keyframes reveal {
+		from {
+			top: 0;
+		}
+		to {
+			top: 100%;
+		}
+	}
+	animation-name: reveal;
+	animation-duration: 2.5s;
+	animation-fill-mode: forwards;
+`;
+
+export const UpperContainer = styled.div`
+	width: 100%;
+	height: 930px;
+	position: absolute;
+	top: -464px;
+	background-color: #fff;
+	left: -296px;
+	max-width: 594px;
+	overflow: hidden;
+	@media (max-width: 580px) {
+		left: -15px;
+	}
+`;
+export const LowerContainer = styled.div`
+	height: 100%;
+	position: absolute;
+	top: 365px;
+	width: 594px;
+	background-color: #fff;
+`;
+
+export const DarkBlueTop = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #003f5a;
+	border-radius: 10%;
+`;
+export const WhiteTop = styled.div`
+	height: 98%;
+	width: 95%;
+	border-style: solid;
+	background-color: #fff;
+
+	border-radius: 10%;
+`;
+export const TealTop = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #007a7a;
+
+	border-radius: 10%;
+`;
+export const CreamTop = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #ebd9c8;
+
+	border-radius: 10%;
+`;
+export const LightOrangeTop = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #fea02f;
+
+	border-radius: 10%;
+`;
+export const OrangeTop = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #de6600;
+	border-radius: 10%;
+`;
+export const DarkBlueBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #003f5a;
+	border-radius: 10%;
+`;
+export const WhiteBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #fff;
+	margin: 0em 0em 0em 3em;
+	border-radius: 10%;
+`;
+export const TealBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #007a7a;
+	border-radius: 10%;
+`;
+export const CreamBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #ebd9c8;
+
+	border-radius: 10%;
+`;
+export const LightOrangeBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #fea02f;
+
+	border-radius: 10%;
+`;
+export const OrangeBottom = styled.div`
+	height: 100%;
+	width: 100%;
+	border-style: solid;
+	background-color: #de6600;
+
+	border-radius: 10%;
+`;
