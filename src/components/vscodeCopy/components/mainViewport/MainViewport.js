ca23029dc518a8f0d84d540f8014f3575e474a02
richanynguon <alina.nguon@gmail.com>
2020-05-23 18:54:25 -0400

    organize imports

--- src/components/vscodeCopy/components/mainViewport/MainViewport.js
+++ src/components/vscodeCopy/components/mainViewport/MainViewport.js
@@ -1,8 +1,12 @@
+// Libraries
 import React from "react";
 
+// Components
 import SideUtilityBar from "./components/sideUtlityBar/SideUtilityBar";
 import ExpandedUtilityBar from "./components/expandedUtilityBar/ExpandedUtilityBar";
 import CodeEditior from "./components/codeEditor/CodeEditior";
+
+// Styles
 import { MainViewContainer } from "./styles/mainViewStyles";
 
 export default function MainViewport() {
