269fffd687774511b12e410a84fecde0bc675177
richanynguon <alina.nguon@gmail.com>
2020-07-05 17:19:36 -0400

    input

--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/topToolBar/styles/searchToolStyles.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/topToolBar/styles/searchToolStyles.js
@@ -2,5 +2,12 @@ import styled from "styled-components";
 export const SearchToolContainer = styled.div`
 	display: flex;
 	align-items: center;
+	justify-content: space-between;
 	height: 3em;
+	margin: 0px 15px 0px 15px;
+	align-items: center;
+`;
+export const IconHolder = styled.div`
+	display: flex;
+	align-items: center;
 `;
