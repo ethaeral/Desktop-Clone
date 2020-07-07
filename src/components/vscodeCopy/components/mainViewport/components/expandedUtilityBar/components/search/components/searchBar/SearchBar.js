1e6e119688c60d83a4bafa474a40b996b59e38fb
richanynguon <alina.nguon@gmail.com>
2020-07-07 11:27:21 -0400

    searchdrop down

--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/searchBar/SearchBar.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/searchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState, useCallback, useRef } from "react";
 import {
 	SearchBarContainer,
 	TagContainer,
@@ -6,24 +6,25 @@ import {
 	DropDownSuggestions,
 	IconHolders,
 	FakeInput,
+	Tag,
+	RemoveTag,
 } from "./styles/searchBarStyles";
 import UtilityIcon from "../../../../../../../utilityIcon/UtilityIcon";
-import Regex from '../../../../../../../../../../assets/utilityBar/searchPerseve.png'
-import Regex2 from '../../../../../../../../../../assets/utilityBar/searchMatchWord.png'
-import Regex3 from '../../../../../../../../../../assets/utilityBar/searchRegex.png'
+import Regex from "../../../../../../../../../../assets/utilityBar/searchPerseve.png";
+import Regex2 from "../../../../../../../../../../assets/utilityBar/searchMatchWord.png";
+import Regex3 from "../../../../../../../../../../assets/utilityBar/searchRegex.png";
 
 function Tags({ tagName, tag, setTag }) {
 	return (
-		<span className='tag'>
+		<Tag>
 			{tagName}
-			<span
+			<RemoveTag
 				onClick={() => {
 					setTag(tag.filter((t) => t !== tagName));
-				}}
-				className='close'>
+				}}>
 				x
-			</span>
-		</span>
+			</RemoveTag>
+		</Tag>
 	);
 }
 
@@ -48,9 +49,13 @@ function Selection({
 }
 
 export default function SearchBar({ stateControl, dependencyMap }) {
+	const BarRef = useRef(null);
+
 	const { state, set } = stateControl;
 	const [tag, setTag] = useState([]);
 	const [suggestions, setSuggestions] = useState([]);
+
+	const [bar, setBar] = useState({ width: 0, height: 0 });
 	const updateTag = useCallback(() => {
 		const foundProjects = tag.map((t) => {
 			return dependencyMap[t];
@@ -69,12 +74,16 @@ export default function SearchBar({ stateControl, dependencyMap }) {
 
 	useEffect(() => {
 		updateTag();
-	}, [updateTag]);
+		setBar({
+			width: BarRef.current.offsetWidth,
+			height: BarRef.current.offsetHeight + BarRef.current.offsetTop,
+		});
+	}, [updateTag, BarRef]);
 
 	return (
 		<SearchBarContainer>
 			<div>
-				<FakeInput>
+				<FakeInput ref={BarRef}>
 					<Input
 						type='text'
 						value={state.input}
@@ -97,19 +106,20 @@ export default function SearchBar({ stateControl, dependencyMap }) {
 						}}
 					/>
 					<IconHolders>
-						<UtilityIcon image ={Regex} hover={Regex} active={Regex}/>
-						<UtilityIcon image ={Regex2} hover={Regex2} active={Regex2}/>
-						<UtilityIcon image ={Regex3} hover={Regex3} active={Regex3}/>
+						<UtilityIcon image={Regex} hover={Regex} active={Regex} />
+						<UtilityIcon image={Regex2} hover={Regex2} active={Regex2} />
+						<UtilityIcon image={Regex3} hover={Regex3} active={Regex3} />
 					</IconHolders>
 				</FakeInput>
 			</div>
-			<TagContainer>
-					{tag.map((t) => (
-						<Tags tagName={t} tag={tag} setTag={setTag} />
-					))}
-				</TagContainer>
+			<TagContainer barWidth={bar.width}>
+				{tag.map((t) => (
+					<Tags tagName={t} tag={tag} setTag={setTag} />
+				))}
+			</TagContainer>
+
 			{suggestions.length > 0 && (
-				<DropDownSuggestions>
+				<DropDownSuggestions barWidth={bar.width-2} height={bar.height}>
 					{suggestions.map((suggestion) => (
 						<Selection
 							selectionName={suggestion}
