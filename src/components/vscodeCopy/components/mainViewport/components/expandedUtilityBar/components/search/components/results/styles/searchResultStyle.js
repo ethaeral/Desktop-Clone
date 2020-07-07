7190963c010de8b740f9c7c3c35b6f81ede81082
richanynguon <alina.nguon@gmail.com>
2020-07-07 11:27:21 -0400

    searchdrop down

--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/results/styles/searchResultStyle.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/results/styles/searchResultStyle.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
 export const SearchResultContainer = styled.div`
-  height: 100%;
+	height: 100%;
+	width: 90%;
+	margin: 0 auto;
 `;
--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/searchBar/styles/searchBarStyles.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/search/components/searchBar/styles/searchBarStyles.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const SearchBarContainer = styled.div`
-	height: 2.5em;
-	margin-top: 5px;
+	margin: 5px 0px 5px 0px;
 `;
 
 export const Input = styled.input`
@@ -11,13 +10,43 @@ export const Input = styled.input`
 	color: #efe8d9;
 	border: none;
 	padding-left: 5px;
-
 `;
 
-export const TagContainer = styled.div``;
+export const TagContainer = styled.div`
+	width: ${(props) => (props.barWidth ? `${props.barWidth}px` : "0px")};
+	display: flex;
+	flex-wrap: wrap;
+	margin: 5px auto;
+`;
 
-export const DropDownSuggestions = styled.div``;
+export const DropDownSuggestions = styled.div`
+	width: ${(props) => (props.barWidth ? `${props.barWidth}px` : "0px")};
+	display: flex;
+	flex-direction: column;
+	margin: 0 auto;
+	background-color: #282a36;
+	border-width: 0px 1px 1px 1px;
+	border-color: #191a21;
+	border-style: solid;
+	z-index: 1;
+	position: absolute;
+	max-height: 200px;
+	overflow-y: scroll;
+	left: calc(3.33% + 4.5em / 2.1);
+	top: ${(props) => (props.height ? `${props.height}px` : "0px")};
+	&::-webkit-scrollbar {
+    background: #282a36;
 
+	}
+	&::-webkit-scrollbar-thumb{
+		background:  rgba(246,246,260,0.1);
+		
+	}
+`;
+// F6  | F6 | F4
+// 15x16 6x1 |  15x16 6x1 |  15x16 4x1
+// 246 | 246 | 250
+// (F6)₁₆ = (15 × 16¹) + (6 × 16⁰) = (246)₁₀
 export const IconHolders = styled.div`
 	display: flex;
 	align-items: center;
@@ -31,4 +60,20 @@ export const FakeInput = styled.div`
 	justify-content: space-between;
 	align-items: center;
 	border: 1px #191a21 solid;
+`;
+
+export const Tag = styled.div`
+	margin-left: 5px;
+	border-radius: 10px;
+	border: 1px #191a21 solid;
+	padding: 5px;
+	display: flex;
+	background-color: #282a36;
+`;
+
+export const RemoveTag = styled.div`
+	margin-left: 10px;
+	cursor: pointer;
+	color: #97618a;
+	font-weight: 700;
 `;
