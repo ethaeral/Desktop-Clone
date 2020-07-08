7a5b480a5169ed87f11eb23805ad3ca02b1f1dce
richanynguon <alina.nguon@gmail.com>
2020-07-08 09:35:26 -0400

    branch components

--- src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/branch/Branch.js
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/branch/Branch.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { BranchSideContainer } from './styles/branchStyles'
+import BranchToolBar from './components/TopToolBar'
+import CommitInput from './components/CommitInput'
+import ChangedSection from './components/ChangedSection'
 
 export default function Branch(props) {
   return (
     <BranchSideContainer active={props.active}>
-      Branch
+     <BranchToolBar/>
+     <CommitInput/>
+     <ChangedSection/>
     </BranchSideContainer>
   )
 }
---
+++ src/components/vscodeCopy/components/mainViewport/components/expandedUtilityBar/components/branch/components/ChangedSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ChangedSection() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
